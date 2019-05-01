67a0f983aa47d1a250b293b7dbcf08d121997831
miroljubtomic <miroljub1995@gmail.com>
2019-05-01 13:50:24 +0200

    MyBrowser added

--- Tools/PlatformWin.cmake
+++ Tools/PlatformWin.cmake
@@ -3,6 +3,7 @@ add_subdirectory(ImageDiff)
 if (ENABLE_WEBKIT_LEGACY)
     add_subdirectory(DumpRenderTree)
     add_subdirectory(MiniBrowser/win)
+	add_subdirectory(MyBrowser/win)
 endif ()
 
 if (ENABLE_WEBKIT)
